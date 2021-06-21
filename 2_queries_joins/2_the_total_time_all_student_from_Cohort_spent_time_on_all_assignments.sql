172959a77f3a0ca0cfe300b1c811f9b52db4b50b
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-06-21 21:26:30 +0000

    2_queries_joins queries over join

---
+++ 2_queries_joins/2_the_total_time_all_student_from_Cohort_spent_time_on_all_assignments.sql
@@ -0,0 +1,4 @@
+SELECT sum(assignment_submissions.duration)as total_duration
+FROM assignment_submissions JOIN students on student_id = students.id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name='FEB12';
