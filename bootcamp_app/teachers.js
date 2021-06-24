11aa194bc8d548d9917649af9ce420087b349a6d
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-06-24 16:18:51 +0000

    added teacher file for connecting js with db using pool

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-console.log("connected!");
-const nameCohort = process.argv[2];
 
-pool.query(
-    `SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort
+const queryString = `SELECT DISTINCT(teachers.name) AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id= assistance_requests.student_id
 JOIN cohorts ON cohorts.id= students.cohort_id
-WHERE cohorts.name = '${nameCohort || 'JUL02'}'
-ORDER BY teacher;`
-  )
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;`;
+
+console.log("connected!");
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}:${user.teacher} `);
